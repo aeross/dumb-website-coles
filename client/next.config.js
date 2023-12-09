bc737b318bdc62681a7d5c3c295820514c04ba38
aeross <andrewpratama22@gmail.com>
2023-12-09 18:46:36 +1100

    deployment config

--- client/next.config.js
+++ client/next.config.js
@@ -3,20 +3,25 @@ const nextConfig = {
     async headers() {
         return [
             {
-                key: "Access-Control-Allow-Origin",
-                value: "https://dumb-website-grocery-store.vercel.app/",
-            },
-            {
-                key: "Access-Control-Allow-Credentials",
-                value: "true",
-            },
-            {
-                key: "Access-Control-Allow-Methods",
-                value: "GET, POST, PATCH, DELETE, PUT, OPTIONS",
-            },
-            {
-                key: "Access-Control-Allow-Headers",
-                value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                source: "/api/:path*",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: "https://dumb-website-grocery-store.vercel.app/",
+                    },
+                    {
+                        key: "Access-Control-Allow-Credentials",
+                        value: "true",
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,POST,PATCH,DELETE,PUT,OPTIONS",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    },
+                ],
             },
         ];
     },
